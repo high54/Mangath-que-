54e2a0e28b77a1e8e009e9adeb581c97d706be43
high54 <julien.bertacco@gmail.com>
2017-04-24 13:59:37 +0200

    Step 1
    Mise en place des models dans Ionic

---
+++ step-1/src/models/serie-model.ts
@@ -0,0 +1,47 @@
+export class SerieModel {
+    /**
+    * Properties
+    */
+    private id: number;
+    private nom: string;
+    private resume: string;
+
+    /**
+    * Getter
+    */
+    public GetId(): number {
+        return this.id;
+    }
+    public GetNom(): string {
+        return this.nom;
+    }
+    public GetResume(): string {
+        return this.resume;
+    }
+
+    /**
+    * Setter
+    */
+    public SetId(id: number) {
+        this.id = id;
+    }
+    public SetNom(nom: string) {
+        this.nnom = nom;
+    }
+    public SetResume(resume: string) {
+        this.resume = resume;
+    }
+    /**
+    * Constructeur vide
+    */
+    public constructor() { }
+
+    public ToString(): string {
+        return this.GetNom() + " <br />"this.GetResume();
+    }
+
+
+
+
+
+}
